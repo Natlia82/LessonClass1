94149a957a40ab2918ae68e3d1fb0c90f5fab143
Natalia <nata@host>
2021-05-28 19:41:56 +0500

    класс готов

---
+++ src/js/character.js
@@ -0,0 +1,46 @@
+class Character {
+    health = 100;
+    level = 1;
+
+    constructor(name, type) {
+        let nameString = new String(name);
+        if (nameString.length >= 2 && nameString.length <= 10) {
+            this.name = nameString;
+        } else throw new Error("Не верный формат имени");
+
+        let mass = ["Bowman", "Swordsman", "Magician", "Daemon", "Undead", "Zombie"];
+        if (mass.includes(type)) {
+            this.type = type;
+        } else throw new Error("Не верный формат типа");
+        switch (this.type) {
+            case "Bowman":
+                this.defence = 25;
+                this.attac = 25;
+                break;
+            case "Swordsman":
+                this.defence = 10;
+                this.attac = 40;
+                break;
+            case "Magician":
+                this.defence = 40;
+                this.attac = 10;
+                break;
+            case "Undead":
+                this.defence = 25;
+                this.attac = 25;
+                break;
+            case "Zombie":
+                this.defence = 10;
+                this.attac = 40;
+                break;
+            case "Daemon":
+                this.defence = 40;
+                this.attac = 10;
+                break;
+
+        }
+    }
+
+
+
+}
